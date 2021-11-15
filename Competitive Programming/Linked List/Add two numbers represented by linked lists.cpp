6d37a7cae3cedb0228ef011753b2dcfc366bb5f6
shreejitverma <shreejiv@andrew.cmu.edu>
2021-11-16 03:44:21 +0530

    Linked list halfway done Nov 16th
    Linked list halfway done Nov 16th update

---
+++ Competitive Programming/Linked List/Add two numbers represented by linked lists.cpp
@@ -0,0 +1,235 @@
+/*
+https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1
+Add two numbers represented by linked lists 
+Easy Accuracy: 30.12% Submissions: 100k+ Points: 2
+Given two numbers represented by two linked lists of size N and M. The task is to return a sum list.
+
+The sum list is a linked list representation of the addition of two input numbers from the last.
+Example 1:
+
+Input:
+N = 2
+valueN[] = {4,5}
+M = 3
+valueM[] = {3,4,5}
+Output: 3 9 0  
+Explanation: For the given two linked
+list (4 5) and (3 4 5), after adding
+the two linked list resultant linked
+list will be (3 9 0).
+Example 2:
+
+Input:
+N = 2
+valueN[] = {6,3}
+M = 1
+valueM[] = {7}
+Output: 7 0
+Explanation: For the given two linked
+list (6 3) and (7), after adding the
+two linked list resultant linked list
+will be (7 0).
+Your Task:
+The task is to complete the function addTwoLists() which has node reference of both the linked lists and returns the head of the sum list.   
+
+Expected Time Complexity: O(N+M)
+Expected Auxiliary Space: O(Max(N,M)) for the resultant list.
+
+Constraints:
+1 <= N, M <= 5000
+*/
+
+// { Driver Code Starts
+// driver
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Linked list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *buildList(int size)
+{
+    int val;
+    cin >> val;
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+void printList(Node *n)
+{
+    while (n)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+// } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+// { Driver Code Starts
+// driver
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Linked list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *buildList(int size)
+{
+    int val;
+    cin >> val;
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+void printList(Node *n)
+{
+    while (n)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+// } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+public:
+    //Function to add two numbers represented by linked list.
+    struct Node *addTwoLists(struct Node *first, struct Node *second)
+    {
+        // code here
+        struct Node *res = NULL; // res is head node of the resultant list
+        struct Node *temp, *prev = NULL;
+        int carry = 0, sum;
+
+        while (first != NULL || second != NULL) //while both lists exist
+        {
+            // Calculate value of next digit in resultant list.
+            // The next digit is sum of following things
+            // (i)  Carry
+            // (ii) Next digit of first list (if there is a next digit)
+            // (ii) Next digit of second list (if there is a next digit)
+            sum = carry + (first ? first->data : 0) + (second ? second->data : 0);
+
+            // update carry for next calulation
+            carry = (sum >= 10) ? 1 : 0;
+
+            // update sum if it is greater than 10
+            sum = sum % 10;
+
+            // Create a new node with sum as data
+            temp = new Node(sum);
+
+            // if this is the first node then set it as head of the resultant list
+            if (res == NULL)
+                res = temp;
+            else // If this is not the first node then connect it to the rest.
+                prev->next = temp;
+
+            // Set prev for next insertion
+            prev = temp;
+
+            // Move first and second pointers to next nodes
+            if (first)
+                first = first->next;
+            if (second)
+                second = second->next;
+        }
+
+        if (carry > 0)
+            temp->next = new Node(carry);
+
+        // return head of the resultant list
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+
+        cin >> n;
+        Node *first = buildList(n);
+
+        cin >> m;
+        Node *second = buildList(m);
+        Solution ob;
+        Node *res = ob.addTwoLists(first, second);
+        printList(res);
+    }
+    return 0;
+}
+// } Driver Code Ends
