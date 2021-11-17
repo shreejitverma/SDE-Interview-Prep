d282675e530826bdfdf8fa0739bfa3534e694803
shreejitverma <shreejiv@andrew.cmu.edu>
2021-11-17 08:33:30 +0530

    Array completed Nov 17th
    Array completed Nov 17th update

---
+++ Competitive Programming/Array/Chocolate Distribution Problem.py
@@ -0,0 +1,76 @@
+'''https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+Chocolate Distribution Problem 
+Easy Accuracy: 53.25% Submissions: 30711 Points: 2
+Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
+
+Example 1:
+
+Input:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between 
+maximum chocolates and minimum chocolates 
+is 9 - 3 = 6 by choosing following M packets :
+{3, 4, 9, 7, 9}.
+Example 2:
+
+Input:
+N = 7, M = 3
+A = {7, 3, 2, 4, 9, 12, 56}
+Output: 2
+Explanation: The minimum difference between
+maximum chocolates and minimum chocolates
+is 4 - 2 = 2 by choosing following M packets :
+{3, 2, 4}.
+Your Task:
+You don't need to take any input or print anything. Your task is to complete the function findMinDiff() which takes array A[ ], N and M as input parameters and returns the minimum possible difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student.
+
+Expected Time Complexity: O(N*Log(N))
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 105
+1 ≤ Ai ≤ 109
+1 ≤ M ≤ N'''
+
+# User function Template for python3
+
+
+class Solution:
+
+    def findMinDiff(self, A, N, M):
+
+        # code here
+        A.sort()  # sorting
+        best = float('inf')  # a very large number
+
+        if N == M:  # corner case
+            return A[N-1] - A[0]
+
+        for i in range(N-M+1):
+            best = min(A[M+i-1] - A[i], best)  # slidig window
+
+        return best
+
+# {
+#  Driver Code Starts
+# Initial Template for Python 3
+
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        N = int(input())
+        A = [int(x) for x in input().split()]
+        M = int(input())
+
+        solObj = Solution()
+
+        print(solObj.findMinDiff(A, N, M))
+# } Driver Code Ends
