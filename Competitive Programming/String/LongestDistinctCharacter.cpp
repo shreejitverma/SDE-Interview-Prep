3324c516d8908313ba5a8cf13d27fb11ac2ec107
shreejitverma <shreejiv@andrew.cmu.edu>
2021-12-16 01:56:51 +0530

    15th Dec
    15th Dec Update

--- Competitive Programming/String/LongestDistinctCharacter.cpp
+++ Competitive Programming/String/LongestDistinctCharacter.cpp
@@ -26,3 +26,31 @@ int main(){
     }
 	return 0;
 }
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> chars(128);
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s[right];
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s[left];
+                chars[l]--;
+                left++;
+            }
+
+            res = max(res, right - left + 1);
+
+            right++;
+        }
+
+        return res;
+    }
+};
