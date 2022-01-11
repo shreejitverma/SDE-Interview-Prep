9c2182564c1a04e0c211479a8c8552f3589c4684
shreejitverma <shreejiv@andrew.cmu.edu>
2022-01-11 20:02:05 +0530

    Python in Depth
    A comprehensive walkthrough of Python.

---
+++ Programming/python/python in depth/String Interpolation/test.py
@@ -0,0 +1,9 @@
+name = 'Shreejit'
+age = 25
+
+# greeting = 'My name is ' + name + ' and I am ' + str(age) + ' years old'
+
+greeting = 'I am {age} years old and my name is {name}'.format(name=name, age=age)
+
+print greeting
+
