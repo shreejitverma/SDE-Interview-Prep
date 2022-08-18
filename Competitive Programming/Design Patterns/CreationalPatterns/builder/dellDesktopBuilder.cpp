ce7d93e4295fbea905f8ba281db136018f13d62f
shreejitverma <shreejiv@andrew.cmu.edu>
2022-08-19 04:05:17 +0530

    System Design Update
    New material added, plus restructuring

---
+++ Competitive Programming/Design Patterns/CreationalPatterns/builder/dellDesktopBuilder.cpp
@@ -0,0 +1,30 @@
+#include "dellDesktopBuilder.hpp"
+
+void DellDesktopBuilder::buildMonitor()
+{
+    desktop->setMonitor("Dell Monitor");
+}
+void DellDesktopBuilder::buildKeyboard()
+{
+    desktop->setKeyboard("Dell Keyboard");
+}
+void DellDesktopBuilder::buildMouse()
+{
+    desktop->setMouse("Dell Mouse");
+}
+void DellDesktopBuilder::buildSpeaker()
+{
+    desktop->setSpeaker("Dell Speaker");
+}
+void DellDesktopBuilder::buildRam()
+{
+    desktop->setRam("Dell Ram");
+}
+void DellDesktopBuilder::buildProcessor()
+{
+    desktop->setProcessor("Dell Processor");
+}
+void DellDesktopBuilder::buildMotherboard()
+{
+    desktop->setMotherboard("Dell Motherboard");
+}
