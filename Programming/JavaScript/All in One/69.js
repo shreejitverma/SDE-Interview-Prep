737d737e7ac62a4ad621685dd6e6e38f4b8ac9c6
shreejitverma <shreejiv@andrew.cmu.edu>
2022-10-20 06:14:21 +0530

    System Design Update

--- Programming/JavaScript/All in One/69.js
+++ Programming/JavaScript/All in One/69.js
@@ -10,6 +10,6 @@ const obj = {
 // const obj2 = {'key69': "value69",...obj};
 // const obj2 = Object.assign({'key69': "value69"}, obj);
 // obj.key3 = "value3";
-// console.log(obj);
+console.log(obj);
 // console.log(obj2);
 
