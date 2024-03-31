417dd5e855742fa311776b6984961df24d80b83d
shreejitverma <shreejitverma@gmail.com>
2024-04-01 04:05:22 +0530

    restructuring System Design deletion Part 1

--- System Design/Design Patterns/In JavaScript/Facade Pattern/README.md
+++
@@ -1,4 +0,0 @@
-# Facade Pattern
-Description Coming Soon
-
-Video: https://youtu.be/fHPa5xzbpaA
--- System Design/Design Patterns/In JavaScript/Singleton Pattern/before/firstUse.js
+++
@@ -1,9 +0,0 @@
-import FancyLogger from './fancyLogger.js'
-
-const logger = new FancyLogger()
-
-export default function logFirstImplementation() {
-  logger.printLogCount()
-  logger.log('First file')
-  logger.printLogCount()
-}
